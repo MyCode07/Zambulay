3cfb7b2a31c268a2370b01c4086411d36ff63b59
MyCode07 <erem19992015@mail.ru>
2025-10-04 15:31:31 +0300

    finish home page

--- src/js/parts/menu.js
+++ src/js/parts/menu.js
@@ -2,16 +2,21 @@ import { lockPadding, unLockPadding } from '../utils/lockPadding.js';
 
 const burger = document.querySelector('.header__burger');
 const menu = document.querySelector('.menu');
-const searchPopup = document.querySelector('.search');
 
 
 document.addEventListener('click', function (e) {
     let targetEl = e.target;
 
     if (targetEl.classList.contains('header__burger')) {
-        burger.classList.add('_active');
-        menu.classList.add('_open');
-        lockPadding();
+        burger.classList.toggle('_active');
+        menu.classList.toggle('_open');
+
+        if (menu.classList.contains('_open')) {
+            lockPadding();
+
+        } else {
+            unLockPadding();
+        }
     }
 
     if (targetEl.classList.contains('menu-close')) {
@@ -20,15 +25,9 @@ document.addEventListener('click', function (e) {
         unLockPadding();
     }
 
-    if (targetEl.hasAttribute('data-open-search')) {
-        searchPopup.classList.add('_open');
-    }
-
-    if (targetEl.hasAttribute('data-close-search')) {
-        searchPopup.classList.remove('_open');
-    }
-
-    if (!targetEl.hasAttribute('data-open-search') && !targetEl.closest('.search') && searchPopup && searchPopup.classList.contains('_open')) {
-        searchPopup.classList.remove('_open');
+    if (targetEl.tagName == 'A' && targetEl.closest('.menu') && document.querySelector('.menu._open')) {
+        burger.classList.remove('_active');
+        menu.classList.remove('_open');
+        unLockPadding();
     }
 })
