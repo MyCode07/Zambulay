a9ff233d9a9419da29f5570a4fc94e092a5f7b94
MyCode07 <erem19992015@mail.ru>
2025-10-06 22:41:11 +0300

    add calc blocks

--- src/js/app.js
+++ src/js/app.js
@@ -1,27 +1,11 @@
-// import { maskInputs } from "./static/inputmask.js";
- import "./parts/menu.js";
-// import "./parts/popup.js";
-// import "./parts/sliders.js";
-// import "./parts/catalog.js";
-// import "./parts/show-more.js";
-// import { accorden } from "./static/accordeon.js";
-// import { runTicker } from "./static/ticker.js";
-// import { Fancybox } from "@fancyapps/ui";
-// // import { animateOrder } from "./parts/animations.js";
-
+import "./parts/menu.js";
 import "./parts/tabs.js";
+import "./parts/calc.js";
 import "./parts/select.js";
 import { stickyHeader } from "./parts/header.js";
 
 stickyHeader();
-// // animateOrder();
-// accorden();
-// runTicker()
 
-// maskInputs('+7 (999) 999-99-99', '._mask-phone')
-
-// Fancybox.bind("[data-fancybox]", {
-// });
 
 document.addEventListener('click', function (e) {
     let targetEl = e.target;
--- src/js/parts/select.js
+++ src/js/parts/select.js
@@ -1,12 +1,22 @@
 const selects = document.querySelectorAll('select');
 if (selects) {
     selects.forEach(item => {
+        let isOpen = false;
+
         item.addEventListener('change', function () {
             if (this.value === "") {
                 this.classList.remove('_active');
+                item.closest('div').classList.remove('_open');
+
             } else {
                 this.classList.add('_active');
+                item.closest('div').classList.add('_open');
             }
+        });
+
+
+        item.addEventListener('click', function () {
+            item.closest('div').classList.toggle('_open');
         });
     })
 }
