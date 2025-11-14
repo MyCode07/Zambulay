e85f753fc669f34a584f3d2408714eb86b3861e0
MyCode07 <erem19992015@mail.ru>
2025-11-14 11:14:41 +0300

    add new card service page

--- src/js/app.js
+++ src/js/app.js
@@ -5,10 +5,11 @@ import "./parts/popup.js";
 import "./parts/select.js";
 import { stickyHeader } from "./parts/header.js";
 import { toTop } from "./static/to-top.js";
+import { accorden } from "./static/accordeon.js";
 
 stickyHeader();
 toTop();
-
+accorden();
 
 document.addEventListener('click', function (e) {
     let targetEl = e.target;
